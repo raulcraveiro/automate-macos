6a43c6f5eaed28873a1e11ae86d05ded9d31fd0b
Raul Craveiro <raulcraveiro@gmail.com>
2022-06-06 19:19:45 -0300

    Update script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Automate macOS
+
+Este script foi criado para que eu consiga automatizar a instalação dos meus softwares após uma instalação do macOS.
--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-CURRDIR=`pwd`
-BREWINSTALLED=`which brew`
-XCODEINSTALLED=`which xcode-select`
+CURRDIR="pwd"
+BREWINSTALLED="which brew"
+XCODEINSTALLED="which xcode-select"
 
 # Instalar o XCode
 if [[ ${XCODEINSTALLED} == "" ]]; then
@@ -15,12 +15,14 @@ if [[ ${BREWINSTALLED} == "" ]]; then
   echo "Instalando o homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   #Adicionando o Homebrew no PATH para Apple Silicon
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  echo "eval '$(/opt/homebrew/bin/brew shellenv)'" >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-#Instalar o repositório Cask
+#Instalar repositórios
 brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew tap homebrew/cask-drivers
 
 #Instalar o MAS para instalar softwares da Mac App Store
 brew install mas
@@ -59,12 +61,14 @@ brew install --cask todoist
 brew install --cask visual-studio-code
 brew install --cask whatsapp
 brew install --cask blackhole-2ch
+brew install --cask logitech-g-hub
 brew install docker
 brew install node
 brew install neofetch
 brew install scrcpy
+brew install yt-dlp
 
-#Instalar todos os aplicativos da MAS
+#Instalar todos os aplicativos da Mac App Store
 mas install 1569813296  # 1Password for Safari
 mas install 1423210932  # Flow 
 mas install 1545870783  # Color Picker
@@ -76,7 +80,70 @@ mas install 409203825   # Numbers
 mas install 1289583905  # Pixelmator Pro 
 mas install 1278508951  # Trello
 
-# Softwares que não tem como ser instalados via script
+#Instalar todas as fontes
+brew install font-bebas-neue 
+brew install font-fira-code
+brew install font-fira-mono
+brew install font-fira-sans
+brew install font-fira-sans-condensed 
+brew install font-fira-sans-extra-condensed 
+brew install font-ibm-plex
+brew install font-ibm-plex-mono 
+brew install font-ibm-plex-sans  
+brew install font-ibm-plex-sans-condensed   
+brew install font-ibm-plex-serif 
+brew install font-inter 
+brew install font-lato  
+brew install font-merriweather
+brew install font-merriweather-sans
+brew install font-montserrat
+brew install font-noto-mono
+brew install font-noto-sans
+brew install font-noto-serif
+brew install font-nunito
+brew install font-nunito-sans
+brew install font-open-sans
+brew install font-open-sans-condensed
+brew install font-oswald
+brew install font-playfair-display
+brew install font-poppins
+brew install font-pt-sans
+brew install font-pt-mono
+brew install font-pt-serif
+brew install font-raleway
+brew install font-roboto
+brew install font-roboto-mono
+brew install font-roboto-serif
+brew install font-rubik
+brew install font-source-sans-pro
+brew install font-source-serif-pro
+brew install font-ubuntu
+brew install font-ubuntu-condensed
+brew install font-ubuntu-mono
+brew install font-work-sans
 
+## Softwares que não tem como ser instalados via script
 #iOS: 17Track, ACNH.Guide e Trakt
-#macOS: Davinci Resolve, PS Remote Play, Tinkertool e Logitech G Hub
+#macOS: Davinci Resolve, PS Remote Play, Tinkertool
+
+#Instalar o Oh My Zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Fazer o Oh My Zsh atualizar automaticamente
+echo "zstyle ':omz:update' mode auto" >> ~/.zshrc
+
+#Fazer o Homebrew atualizar automaticamente 
+brew autoupdate start --upgrade --cleanup
+
+#Mudar o emoji do Homebrew, só pra deixar mais personalizado
+export HOMEBREW_INSTALL_BADGE="🎉"
+
+#Adicionar o comando "code ." no terminal
+cat << EOF >> ~/.zprofile
+# Add Visual Studio Code (code)
+export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
+
+#Limpar o que restou das instalações
+brew autoremove
+brew cleanup
